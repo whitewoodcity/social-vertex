59efdffdbc95ee91baf922dcb5a3ab2773ad931e
zxj5470 <zxj5470@foxmail.com>
2018-07-08 16:15:17 +0800

    add secondVerticle and its test. Run with `vertx.deployVerticle(SecondVerticle.class.name)` in groovy

--- src/main/groovy/cn/net/polyglot/MainVerticle.groovy
+++ src/main/groovy/cn/net/polyglot/MainVerticle.groovy
@@ -4,10 +4,6 @@ import io.vertx.core.AbstractVerticle
 
 class MainVerticle extends AbstractVerticle {
   void start() {
-    vertx.createHttpServer().requestHandler { req ->
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello World!")
-    }.listen(8080)
+    vertx.deployVerticle(SecondVerticle.class.name)
   }
 }
