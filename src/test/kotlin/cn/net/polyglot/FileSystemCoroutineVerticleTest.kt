61f9c20b719358f4f851378f025fa2d086615b42
zxj5470 <zxj5470@foxmail.com>
2018-07-09 13:04:38 +0800

    rename verticles

--- src/test/kotlin/cn/net/polyglot/FileSystemCoroutineVerticleTest.kt
+++ src/test/kotlin/cn/net/polyglot/FileSystemCoroutineVerticleTest.kt
@@ -1,7 +1,6 @@
 package cn.net.polyglot
 
 import cn.net.polyglot.testframework.VertxTestBase
-import cn.net.polyglot.testframework.shouldBe
 import io.vertx.ext.unit.TestContext
 import org.junit.Test
 
@@ -10,23 +9,27 @@ import org.junit.Test
  * @author zxj5470
  * @date 2018/7/8
  */
-class SecondVerticleTest2 : VertxTestBase() {
-  override var currentPort = 8088
+class FileSystemCoroutineVerticleTest : VertxTestBase() {
+  override var currentPort = 8083
 
   init {
-  	setVerticle<SecondVerticle>()
+  	setVerticle<FileSystemCoroutineVerticle>()
   }
 
   @Test
   override fun testApplication(context: TestContext) {
     val async = context.async()
+    val allContent = StringBuilder()
     vertx.createHttpClient().getNow(currentPort, "localhost", "/") { response ->
+
       response.handler { body ->
-        println("content:")
-        println(body.toString())
+        val ret = body.bytes.toKString()
+        allContent.append(ret)
+      }
 
-        body.toString().contains("Hello") shouldBe true
-
+      response.endHandler {
+        println(allContent)
+        context.assertTrue(allContent.contains("ConfigLoader"))
         async.complete()
       }
     }
