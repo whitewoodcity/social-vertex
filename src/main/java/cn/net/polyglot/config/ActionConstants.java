67777a840fb48728b63e15648d2e41b9880e8c20
zxj5470 <zxj5470@foxmail.com>
2018-07-12 03:18:06 +0800

    Use Java File for Constants to the benefit of Kotlin File `import xxxxx.*` which Kotlin can't do with `object` + `const val`.

---
+++ src/main/java/cn/net/polyglot/config/ActionConstants.java
@@ -0,0 +1,10 @@
+package cn.net.polyglot.config;
+
+public class ActionConstants {
+  public static final String REQUEST = "request";
+  public static final String RESPONSE = "response";
+  public static final String DELETE = "delete";
+
+  public static final String LOGIN = "login";
+  public static final String REGISTRY = "registry";
+}
