71f545d02378545f6bb03898f08abd644b7a1342
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2018-07-18 10:30:58 +0800

    add handlers interface and implemented class

--- src/main/kotlin/cn/net/polyglot/handler/IMMessageHandlers.kt
+++ src/main/kotlin/cn/net/polyglot/handler/IMMessageHandlers.kt
@@ -3,10 +3,22 @@ package cn.net.polyglot.handler
 import io.vertx.core.file.FileSystem
 import io.vertx.core.json.JsonObject
 
-interface IMMessageHandlers {
-  fun register(fs: FileSystem, user:String):JsonObject
-  fun searchUser(fs: FileSystem, json: JsonObject):JsonObject
-  fun message(fs: FileSystem, userFile: String, id: String?, crypto: String?, json: JsonObject):JsonObject
-  fun friend(fs: FileSystem, from:String, to: String):JsonObject
-  fun deleteFriend(fs: FileSystem, from:String, to: String):JsonObject
+fun register(fs: FileSystem, user: String): JsonObject {
+  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun searchUser(fs: FileSystem, json: JsonObject): JsonObject {
+  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun message(fs: FileSystem, userFile: String, id: String?, crypto: String?, json: JsonObject): JsonObject {
+  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun friend(fs: FileSystem, from: String, to: String): JsonObject {
+  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun deleteFriend(fs: FileSystem, from: String, to: String): JsonObject {
+  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
