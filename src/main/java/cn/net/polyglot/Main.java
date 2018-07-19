8fbe5db69e8d949cfc654e8437569c540e184a6b
zxj5470 <zxj5470@foxmail.com>
2018-07-19 10:12:59 +0800

    update

--- src/main/java/cn/net/polyglot/Main.java
+++ src/main/java/cn/net/polyglot/Main.java
@@ -4,7 +4,6 @@ import io.vertx.core.Launcher;
 
 import static cn.net.polyglot.config.FileSystemConstants.MAIN_VERTICLE;
 
-
 public class Main {
   public static void main(String[] args) {
     Launcher.executeCommand("run", MAIN_VERTICLE);
