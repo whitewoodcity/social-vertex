cb9e9aa2361690d7610ce4c3d54f7d3c4e1c1ce6
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2018-07-22 07:58:08 +0800

    增加dir文件夹的路径，dir表示数据所在文件夹的位置，缺省是jar所在文件夹 修改写法，用merge in函数将读取来的config替换缺省的配置

--- src/main/groovy/cn/net/polyglot/main_verticle.groovy
+++ src/main/groovy/cn/net/polyglot/main_verticle.groovy
@@ -6,13 +6,17 @@ import io.vertx.config.ConfigRetriever
 import io.vertx.config.ConfigRetrieverOptions
 import io.vertx.config.ConfigStoreOptions
 import io.vertx.core.DeploymentOptions
+import io.vertx.core.Launcher
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
 
 Vertx vertx = vertx
 
-final int DEFAULT_PORT = 8080
-JsonObject defaultConfig = new JsonObject().put("port", DEFAULT_PORT)
+JsonObject config = new JsonObject()
+  .put("port", 8080)
+  .put("dir", new File(Launcher.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent())
+
+println config
 
 ConfigStoreOptions fileStore = new ConfigStoreOptions()
   .setType("file")
@@ -24,24 +28,14 @@ ConfigRetriever retriever = ConfigRetriever.create(vertx, options)
 
 retriever.getConfig { ar ->
 
-  if (ar.failed()) {
-
+  if(ar.succeeded()){
+    config.mergeIn(ar.result())
+  }else{
     System.out.println("The configuration file: config.json does not exist or in wrong format, use default config.")
-    deployVerticles(defaultConfig)
-
-  } else {
-
-    JsonObject config = ar.result()
-    int port = config.getInteger("port", DEFAULT_PORT)
-
-    if (0 <= port && port < 65536) {
-      deployVerticles(config)
-    } else {
-      System.out.println("The configuration file: config.json does not exist or in wrong format, use default config.")
-      deployVerticles(defaultConfig)
-    }
-
   }
+
+  deployVerticles(config)
+
 }
 
 private void deployVerticles(JsonObject config) {
