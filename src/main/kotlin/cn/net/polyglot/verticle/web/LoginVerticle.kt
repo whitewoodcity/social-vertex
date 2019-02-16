433507e341c6dcf6f6a328301b10fd604e0fdbe5
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2019-02-16 20:21:22 +0800

    replace deprecated method

--- src/main/kotlin/cn/net/polyglot/verticle/web/LoginVerticle.kt
+++ src/main/kotlin/cn/net/polyglot/verticle/web/LoginVerticle.kt
@@ -5,8 +5,10 @@ import cn.net.polyglot.module.md5
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.core.file.existsAwait
+import io.vertx.kotlin.core.file.readDirAwait
 import io.vertx.kotlin.core.file.readFileAwait
 import java.io.File
+import java.io.File.separator
 
 class LoginVerticle : ServletVerticle() {
   override suspend fun start() {
@@ -18,7 +20,7 @@ class LoginVerticle : ServletVerticle() {
     val id = json.getJsonObject(FORM_ATTRIBUTES).getString(ID)
     val password = json.getJsonObject(FORM_ATTRIBUTES).getString(PASSWORD)
 
-    var verified = true
+    var verified = false
     if(vertx.fileSystem().existsAwait(config.getString(DIR) + File.separator + id+ File.separator + "user.json")){
       val fileJson = vertx.fileSystem().readFileAwait(config.getString(DIR) + File.separator + id+ File.separator + "user.json").toJsonObject()
       if(fileJson.getString(PASSWORD) == md5(password))
@@ -29,15 +31,29 @@ class LoginVerticle : ServletVerticle() {
       session.put(ID, id)
       JsonObject().put(VALUES,
           JsonObject()
-            .put("username", json.getJsonObject(FORM_ATTRIBUTES).getString("id"))
-            .put("friends", JsonArray().add("1").add("2"))
+            .put("username", id)
+            .put("friends", retrieveFriends(id))
         )
         .put(TEMPLATE_PATH, "sample/result.html")
     }else{
       JsonObject()
         .put(TEMPLATE_PATH, "index.htm")
     }
+  }
 
+  private suspend fun retrieveFriends(id:String):JsonArray{
+    val friends = JsonArray()
 
+    val dir = config.getString(DIR)
+
+    if(vertx.fileSystem().existsAwait("$dir$separator$id")){
+      val list = vertx.fileSystem().readDirAwait("$dir$separator$id")
+      for(string in list){
+        if(!string.contains("."))
+          friends.add(string)
+      }
+    }
+
+    return friends
   }
 }
