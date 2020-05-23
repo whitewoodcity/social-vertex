902417b1fbd6f7148be54911b494256041befc9d
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2020-05-23 16:04:56 +0800

    add svg to static file suffix list

--- src/main/kotlin/cn/net/polyglot/verticle/WebServerVerticle.kt
+++ src/main/kotlin/cn/net/polyglot/verticle/WebServerVerticle.kt
@@ -6,27 +6,27 @@ import cn.net.polyglot.verticle.im.IMServletVerticle
 import cn.net.polyglot.verticle.web.DispatchVerticle
 import io.vertx.core.http.HttpMethod
 
-class WebServerVerticle: DispatchVerticle() {
+class WebServerVerticle : DispatchVerticle() {
 
   //设置静态文件后缀，path若是这些后缀，则自动读取对应路径文件予以返回
   override fun initDispatchVerticle() {
     super.initDispatchVerticle()//.htm
-    staticFileSuffix.addAll(setOf("ico", "css", "js","text","png","jpg","gif","jpeg","mp3","avi","mp4"))
+    staticFileSuffix.addAll(setOf("ico", "css", "js", "text", "png", "jpg", "gif", "jpeg", "mp3", "avi", "mp4", "svg"))
   }
 
   //设置http方法为put时候，http请求体缺省为application/json，但是flutter中以及需要设置Content-Type为application/json，否则会自动设为text/plain
-  override fun getDefaultContentTypeByHttpMethod(httpMethod: HttpMethod):String{
-    return when(httpMethod){
+  override fun getDefaultContentTypeByHttpMethod(httpMethod: HttpMethod): String {
+    return when (httpMethod) {
       HttpMethod.PUT -> "application/json"
       else -> super.getDefaultContentTypeByHttpMethod(httpMethod)
     }
   }
 
   override suspend fun getVerticleAddressByPath(httpMethod: HttpMethod, path: String): String {
-    return when(httpMethod){
-      HttpMethod.GET, HttpMethod.POST -> when(path){
-        "/login","/profile","/register","/update","/portrait" -> LoginVerticle::class.java.name
-        "/prepareArticle", "/submitArticle","/prepareModifyArticle","/modifyArticle","/deleteArticle","/prepareSearchArticle","/searchArticle","/article", "/community","/uploadPortrait" -> CommunityVerticle::class.java.name
+    return when (httpMethod) {
+      HttpMethod.GET, HttpMethod.POST -> when (path) {
+        "/login", "/profile", "/register", "/update", "/portrait" -> LoginVerticle::class.java.name
+        "/prepareArticle", "/submitArticle", "/prepareModifyArticle", "/modifyArticle", "/deleteArticle", "/prepareSearchArticle", "/searchArticle", "/article", "/community", "/uploadPortrait" -> CommunityVerticle::class.java.name
         else -> ""
       }
       HttpMethod.PUT -> IMServletVerticle::class.java.name
